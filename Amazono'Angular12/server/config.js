315642e98d5eff1a711e1561acc063ed0f6a6250
sikandar44 <meersikanderali44@gmail.com>
2020-11-13 23:21:36 +0500

    amazone signUp login profile

---
+++ Amazono'Angular12/server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database:
+'mongodb+srv://meersikanderali44:03124196671@mongodb.xhhet.mongodb.net/meersikanderali44?retryWrites=true&w=majority',
+    port: 3030,
+   
+    
+};
